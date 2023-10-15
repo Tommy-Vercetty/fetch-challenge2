81aee066aa32432c07f30419a11bec2208f0641c
Enrico Ortenzi <b00156016@mytudublin.ie>
2023-10-15 13:50:49 +0100

    New Update

---
+++ Play Fetch Challenge 2 Submission/Assets/Challenge 2/Scripts/OpenLink.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OpenLink : MonoBehaviour
+{
+
+    public void OpenChannel() {
+            Application.OpenURL("https://github.com/Tommy-Vercetty/fetch-challenge2");
+        }
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
